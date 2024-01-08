6a679d0c9bc1eb2dfb4a66b5aeffcf48a041e2f9
Curtis Shieh <xzblogin@163.com>
2024-01-08 16:25:46 +0800

    Update script.js 搜索=百度

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 // You can add JavaScript functionality here if needed
 // Example: Adding a simple image carousel
+document.getElementById("searchButton").addEventListener("click", function() {
+  // 获取输入框的值
+  var searchTerm = document.getElementById("searchInput").value;
 
+  // 使用百度搜索
+  window.location.href = "https://www.baidu.com/s?wd=" + encodeURIComponent(searchTerm);
+});
 
